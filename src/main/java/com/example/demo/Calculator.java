60930cdccd2e35ba64b621e3ffc57d0d3542e50f
phadatul <phadatul@gmail.com>
2024-10-26 16:54:13 +0530

    adding tests

---
+++ src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+	public int sub(int a,int b) {
+		return a-b;
+	}
+
+}
--- src/test/java/com/example/demo/SkillupjavaApplicationTests.java
+++ src/test/java/com/example/demo/SkillupjavaApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SkillupjavaApplicationTests {
+	@Autowired
+	Calculator c;
 
 	@Test
-	void contextLoads() {
+	public void testAdd() {
+
+		Assertions.assertEquals(12, c.add(10, 2));
+	}
+	
+	@Test
+	public void testSub() {
+
+		Assertions.assertEquals(8, c.sub(10, 2));
 	}
 
 }
